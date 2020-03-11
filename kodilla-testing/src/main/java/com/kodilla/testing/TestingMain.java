64ee836f4633fed7c1240611351b83418aded845
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-03-11 09:14:59 +0100

    Calculator test

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,39 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator();
+        int add = calculator.add(2,7);
+        int subtract = calculator.subtract(10,9);
+
+        System.out.println("\nTesting add() from com.kodilla.testing.calculator.Calculator:");
+        if (add == 9) {
+            System.out.println("add() test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("\nTesting subtract() from com.kodilla.testing.calculator.Calculator:");
+        if (subtract == 1) {
+            System.out.println("subtract() test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+
+
     }
 }
