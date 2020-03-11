8874d645b2f4fed20f50932be4f14389a2880179
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-03-11 13:17:45 +0100

    OddNumbersExterminatorTest

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -5,33 +5,33 @@ import com.kodilla.testing.calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
-
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")) {
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        Calculator calculator = new Calculator();
-        int add = calculator.add(2,7);
-        int subtract = calculator.subtract(10,9);
-
-        System.out.println("\nTesting add() from com.kodilla.testing.calculator.Calculator:");
-        if (add == 9) {
-            System.out.println("add() test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        System.out.println("\nTesting subtract() from com.kodilla.testing.calculator.Calculator:");
-        if (subtract == 1) {
-            System.out.println("subtract() test OK");
-        } else {
-            System.out.println("Error!");
-        }
+//        SimpleUser simpleUser = new SimpleUser("theForumUser");
+//
+//        String result = simpleUser.getUsername();
+//
+//        if (result.equals("theForumUser")) {
+//            System.out.println("test OK");
+//        } else {
+//            System.out.println("Error!");
+//        }
+//
+//        Calculator calculator = new Calculator();
+//        int add = calculator.add(2,7);
+//        int subtract = calculator.subtract(10,9);
+//
+//        System.out.println("\nTesting add() from com.kodilla.testing.calculator.Calculator:");
+//        if (add == 9) {
+//            System.out.println("add() test OK");
+//        } else {
+//            System.out.println("Error!");
+//        }
+//
+//        System.out.println("\nTesting subtract() from com.kodilla.testing.calculator.Calculator:");
+//        if (subtract == 1) {
+//            System.out.println("subtract() test OK");
+//        } else {
+//            System.out.println("Error!");
+//        }
 
 
 
