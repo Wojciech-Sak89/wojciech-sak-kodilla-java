b73b4f8d6319b6420bdbda4d277d841a684d2bcf
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-03-18 23:55:14 +0100

    Filter ForumUsers

--- kodilla-stream/src/main/java/StreamMain.java
+++ kodilla-stream/src/main/java/StreamMain.java
@@ -1,5 +1,9 @@
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.forumuser.Forum;
+import com.kodilla.stream.forumuser.ForumUser;
+
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
@@ -30,13 +34,84 @@ public class StreamMain {
 //        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
 //        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-        poemBeautifier.beautify("pretty text", (a) -> a.toUpperCase());
-        poemBeautifier.beautify("alfabetINeed!!!", (a) -> "ABC " + a + " ABC");
-        poemBeautifier.beautify("CAPS TEXT", (a) -> a.toLowerCase());
-        poemBeautifier.beautify("I DON'T NEED ANY \"A\" LETTERS!!! I NEED \"B\" INSTEAD", (a) -> a.replaceAll("A", "B"));
+//        PoemBeautifier poemBeautifier = new PoemBeautifier();
+//        poemBeautifier.beautify("pretty text", (a) -> a.toUpperCase());
+//        poemBeautifier.beautify("alfabetINeed!!!", (a) -> "ABC " + a + " ABC");
+//        poemBeautifier.beautify("CAPS TEXT", (a) -> a.toLowerCase());
+//        poemBeautifier.beautify("I DON'T NEED ANY \"A\" LETTERS!!! I NEED \"B\" INSTEAD", (a) -> a.replaceAll("A", "B"));
 
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
+//        System.out.println("Using Stream to generate even numbers from 1 to 20");
+//        NumbersGenerator.generateEven(20);
+
+       /* People.getList().stream()
+                .map(String::toUpperCase) // = .map(s -> s.toUpperCase())
+                .forEach(s -> System.out.println(s)); // = .forEach(System.out::println);*/
+
+//        People.getList().stream()
+//                .filter(s -> s.length() > 11)
+//                .forEach(System.out::println);
+
+//        People.getList().stream()
+//                .map(String::toUpperCase)
+//                .filter(s -> s.length() > 11)
+//                .map(s -> s.substring(0, s.indexOf(' ') + 2) + ".")
+//                .filter(s -> s.substring(0, 1).equals("M"))
+//                .forEach(System.out::println);
+
+//        BookDirectory theBookDirectory = new BookDirectory();
+//        theBookDirectory.getList().stream()
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .forEach(System.out::println);
+
+//        BookDirectory theBookDirectory = new BookDirectory();
+//        List<Book> theResultListOfBooks = theBookDirectory.getList().stream()
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .collect(Collectors.toList());
+//
+//        System.out.println("# elements: " + theResultListOfBooks.size());
+//        theResultListOfBooks.stream()
+//                .forEach(System.out::println);
+
+
+
+
+
+
+        //***
+//        BookDirectory theBookDirectory = new BookDirectory();
+//
+//        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .collect(Collectors.toMap(Book::getSignature, book -> book));
+//
+//        System.out.println("# elements: " + theResultMapOfBooks.size());
+//        theResultMapOfBooks.entrySet().stream()
+//                .map(entry -> entry.getKey() + ": " + entry.getValue())
+//                .forEach(System.out::println);
+        //***
+
+        //        BookDirectory theBookDirectory = new BookDirectory();
+//        String theResultStringOfBooks = theBookDirectory.getList().stream()
+//                .filter(book -> book.getYearOfPublication() > 2005)
+//                .map(Book::toString)
+//                .collect(Collectors.joining(",\n","<<",">>"));
+//
+//        System.out.println(theResultStringOfBooks);
+
+        Forum theForum = new Forum();
+        LocalDate now = LocalDate.now();
+        LocalDate twentyYearsAgo = now.minusYears(20);
+
+        Map<Integer, ForumUser> theResultMapOfForumUsers = theForum.getUserList().stream()
+                .filter(forumUser -> forumUser.getSex() == 'M')
+                .filter(forumUser -> (forumUser.getBirthYear().isBefore(twentyYearsAgo)) || (forumUser.getBirthYear().isEqual(twentyYearsAgo)))
+                .filter(forumUser -> forumUser.getPostsPublished() > 0)
+                .collect(Collectors.toMap(ForumUser::getId, forumUser -> forumUser));
+
+        theResultMapOfForumUsers.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
+
+
     }
 }
