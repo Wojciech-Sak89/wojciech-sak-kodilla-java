e0b19adfb925f75fcac739660782e0bfc9c16cd6
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-03-24 21:19:48 +0100

    ExceptionHandling

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -8,7 +8,7 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
 
@@ -17,14 +17,28 @@ public class FileReader {
             fileLines.forEach(System.out::println);
 
         } catch (IOException e) {
-            System.out.println("Oh no! Something went wrong! Error:" + e);
+           throw new FileReaderException();
         } finally {
 
             System.out.println("I am gonna be here... always!");
 
         }
-
-
-//        System.out.println(file.getPath());
     }
+
+//    public void readFile() {
+//        ClassLoader classLoader = getClass().getClassLoader();
+//        File file = new File(classLoader.getResource("file/names.txt").getFile());
+//
+//        try (Stream<String> fileLines = Files.lines(Paths.get(file.getPath()))) {
+//
+//            fileLines.forEach(System.out::println);
+//
+//        } catch (IOException e) {
+//            System.out.println("Oh no! Something went wrong! Error:" + e);
+//        } finally {
+//
+//            System.out.println("I am gonna be here... always!");
+//
+//        }
+//    }
 }
