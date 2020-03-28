c15ce9ba04042de30ba4a0ef58793ba717d94ca3
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-03-28 12:58:33 +0100

    Rock Paper Scissors Spock Lizard

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -7,44 +7,6 @@ public class RpsRunner {
     public static void main(String[] args) {
 //        StartGame startGame = new StartGame();
 //        startGame.start();
-//
-//        GraphicsASCII graphicsASCII = new GraphicsASCII();
-//        graphicsASCII.rockBW();
-//        graphicsASCII.rock256();
-//
-//        graphicsASCII.scissorsBW();
-//        graphicsASCII.scissors256();
-//
-//        graphicsASCII.paperBW();
-//        graphicsASCII.paper256();
-//
-//        graphicsASCII.spockBW();
-//        graphicsASCII.spock256();
-//
-//        graphicsASCII.lizardBW();
-//        graphicsASCII.lizard256();
-//
-//        BannersASCII bannersASCII = new BannersASCII();
-//
-//
-//        bannersASCII.crushesActionText();
-//        bannersASCII.cutsActionText();
-//        bannersASCII.decapitatesActionText();
-//        bannersASCII.coversActionText();
-//        bannersASCII.disprovesActionText();
-//        bannersASCII.smashesActionText();
-//        bannersASCII.vaporizesActionText();
-//        bannersASCII.eatsActionText();
-//        bannersASCII.poisonsActionText();
-//
-//        bannersASCII.scissorsText();
-//        bannersASCII.rockText();
-//        bannersASCII.paperText();
-//        bannersASCII.spockText();
-//        bannersASCII.lizardText();
-
-
-
 
         StartGame_SpockLizardExtension startGame_SpockLizardExtension = new StartGame_SpockLizardExtension();
         startGame_SpockLizardExtension.start();
