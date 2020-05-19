9f20ff27bd0794ce44dac30273af771ba788e2e2
Wojciech-Sak89 <wojciech.r.sak@gmail.com>
2020-05-19 17:15:26 +0200

    flights

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/Main.java
@@ -1,8 +1,10 @@
 package com.kodilla.good.patterns.challenges;
 
-import com.kodilla.good.patterns.challenges.food2door.*;
+import com.kodilla.good.patterns.challenges.flights.AirportsRetriever;
+import com.kodilla.good.patterns.challenges.flights.PolishAirports;
 
 public class Main {
+
     public static void main(String[] args) {
 //        MovieStore movieStore = new MovieStore();
 //
@@ -24,26 +26,39 @@ public class Main {
 //
 //        productOrderService.process(orderRequest);
 
-        Order order1 = new Order("ExtraFoodShop", ShoppingList.first());
-        Order order2 = new Order("GlutenFREEDOOM", ShoppingList.second());
-        Order order3 = new Order("GlutenFreeShop", ShoppingList.second());
-        Order order4 = new Order("HealthyShop", ShoppingList.third());
 
-        ExtraFoodShop extraFoodShop = new ExtraFoodShop();
-        GlutenFreeShop glutenFreeShop = new GlutenFreeShop();
-        HealthyShop healthyShop = new HealthyShop();
+//        Order order1 = new Order("ExtraFoodShop", ShoppingList.first());
+//        Order order2 = new Order("GlutenFREEDOOM", ShoppingList.second());
+//        Order order3 = new Order("GlutenFreeShop", ShoppingList.second());
+//        Order order4 = new Order("HealthyShop", ShoppingList.third());
+//
+//        ExtraFoodShop extraFoodShop = new ExtraFoodShop();
+//        GlutenFreeShop glutenFreeShop = new GlutenFreeShop();
+//        HealthyShop healthyShop = new HealthyShop();
+//
+//        System.out.println("*** ExtraFoodShop Order ***");
+//        extraFoodShop.process(order1);
+//
+//        System.out.println("\n*** GlutenFreeShop Order ...with typo ***");
+//        glutenFreeShop.process(order2);
+//
+//        System.out.println("\n*** GlutenFreeShop Order ***");
+//        glutenFreeShop.process(order3);
+//
+//        System.out.println("\n*** HealthyShop Order ***");
+//        healthyShop.process(order4);
 
-        System.out.println("*** ExtraFoodShop Order ***");
-        extraFoodShop.process(order1);
+        AirportsRetriever airportsRetriever = new AirportsRetriever();
+        PolishAirports polishAirports = new PolishAirports(airportsRetriever.retrieve());
 
-        System.out.println("\n*** GlutenFreeShop Order ...with typo ***");
-        glutenFreeShop.process(order2);
+        polishAirports.printFlightsFrom("Gdańsk");
 
-        System.out.println("\n*** GlutenFreeShop Order ***");
-        glutenFreeShop.process(order3);
+        System.out.println("\n***************\n");
+        polishAirports.printCitiesWithConnectionTo("Gdańsk");
 
-        System.out.println("\n*** HealthyShop Order ***");
-        healthyShop.process(order4);
+        System.out.println("\n***************\n");
+        polishAirports.printConnectingCities("Warszawa", "łódź");
 
     }
+
 }
